f2cb786bf8e30160c506bb3254a2ffa923f2a313
kozhevnikovanataly@yandex.ru <kozhevnikovanataly@yandex.ru>
2022-11-27 15:45:09 +0300

    commit

---
+++ Task5.py
@@ -0,0 +1,13 @@
+import math
+import os
+os.system('cls')
+ax = float(input('Введите координату точки A по оси Х: '))
+ay = float(input('Введите координату точки А по оси Y: '))
+bx = float(input('Введите координату точки B по оси Х: '))
+by = float(input('Введите координату точки B по оси Y: '))
+distance = round(math.sqrt((bx-ax) ** 2 + (by -ay) ** 2):
+    print(distance)
+
+
+
+
